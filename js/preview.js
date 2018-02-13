ed9a2222da9306f8869279ed0c66bd7b15bd254a
dolonche <ivan.pitenin@gmail.com>
2018-02-13 18:55:22 +0300

    error

--- js/preview.js
+++ js/preview.js
@@ -13,31 +13,28 @@
   };
   var pictureElement = document.querySelector('.pictures');
   var galleryClose = gallery.querySelector('.gallery-overlay-close');
-//  for (var k = 0; k < pictureItem.length; k++) {
-//    (function (index) {
-//      pictureItem[k].addEventListener('click', function (event) {
-//        event.preventDefault();
-//        gallery.classList.remove('hidden');
-//        renderGalleryItem(window.data.picData()[index + 1]);
-//      });
-//    })(k);
-//  }
-  pictureElement.addEventListener('click', onClickPicture)
-  function onClickPicture (e) {
-  e.preventDefault()
-  var target = e.target;
-
-
-  for (var i = 0; i < pictureElement.children.length; i++) {
-    if (pictureElement.children[i].querySelector('img') === target) {
-
-      // gallery.appendChild(renderGalleryItem(pictures[i]));
-      console.log(window.data.picData()[i + 1]);
-      gallery.classList.remove('hidden');
-      renderGalleryItem(window.data.picData()[i + 1]);// при клике получаю обьект, по которому кликнул пользователь, твоя задача - этот обьект передать в функцию, которая рендерит картинку с оверлеем
+  //  for (var k = 0; k < pictureItem.length; k++) {
+  //    (function (index) {
+  //      pictureItem[k].addEventListener('click', function (event) {
+  //        event.preventDefault();
+  //        gallery.classList.remove('hidden');
+  //        renderGalleryItem(window.data.picData()[index + 1]);
+  //      });
+  //    })(k);
+  //  }
+  pictureElement.addEventListener('click', onClickPicture);
+  function onClickPicture(e) {
+    e.preventDefault();
+    var target = e.target;
+    for (var i = 0; i < pictureElement.children.length; i++) {
+      if (pictureElement.children[i].querySelector('img') === target) {
+        // gallery.appendChild(renderGalleryItem(pictures[i]));
+        // console.log(window.data.picData()[i + 1]);
+        gallery.classList.remove('hidden');
+        renderGalleryItem(window.data.picData()[i + 1]); // при клике получаю обьект, по которому кликнул пользователь, твоя задача - этот обьект передать в функцию, которая рендерит картинку с оверлеем
+      }
     }
   }
-}
   var closePopup = function () {
     gallery.classList.add('hidden');
   };
